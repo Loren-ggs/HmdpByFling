74019d2a75b19a9e9b31db1bacbfba3acbfb9f09
mopFLiNG <3146856943@qq.com>
2025-10-31 16:11:17 +0800

    feat(user): 实现用户登录与验证码功能-day1
    - 添加发送验证码接口，支持手机号验证和Redis存储
    - 实现用户登录逻辑，包括验证码校验和用户创建
    - 集成登录拦截器，通过Token验证用户身份
    - 配置MVC拦截规则，排除无需登录的路径- 更新Redis常量，增加Token过期时间配置
    - 完善用户信息存储至Redis，使用Hash结构优化性能
    - 增加ThreadLocal工具类，用于保存当前登录用户信息- 修改数据库及Redis连接配置，调整密码和地址参数

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,10 +1,31 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.captcha.generator.RandomGenerator;
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.bean.copier.CopyOptions;
+import cn.hutool.core.lang.UUID;
+import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.LoginFormDTO;
+import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
+import com.hmdp.utils.RegexUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.RedisConstants.*;
+import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
 
 /**
  * <p>
@@ -15,6 +36,81 @@ import org.springframework.stereotype.Service;
  * @since 2021-12-22
  */
 @Service
+@Slf4j
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+    @Override
+    public Result sendCode(String phone, HttpSession session) {
 
+        //校验手机号
+        if(RegexUtils.isPhoneInvalid( phone)){
+            return Result.fail("手机号格式错误");
+        }
+
+        //生产验证码
+        String code = RandomUtil.randomNumbers(6);
+
+        session.setAttribute("code",code);
+        //TODO 调用阿里云短信服务发送验证码、试试邮箱验证码
+
+        stringRedisTemplate.opsForValue().set(LOGIN_CODE_KEY+phone,code, LOGIN_CODE_TTL, TimeUnit.MINUTES);
+
+        log.info("模拟发送验证码成功，验证码为: {}",code);
+        return Result.ok();
+    }
+
+    @Override
+    public Result login(LoginFormDTO loginForm, HttpSession session) {
+        //校验手机号
+        if(RegexUtils.isPhoneInvalid(loginForm.getPhone())){
+            return Result.fail("手机号格式错误");
+        }
+        //校验验证码
+        String cacheCode = stringRedisTemplate.opsForValue().get(LOGIN_CODE_KEY+loginForm.getPhone());
+        if (cacheCode == null){
+            return Result.fail("验证码获取失败");
+        }
+        if(!cacheCode.equals(loginForm.getCode())){
+            return Result.fail("验证码错误");
+        }
+        //验证码一致，查询用户
+        User user = query().eq("phone", loginForm.getPhone()).one();
+        //用户不存在,新建一个
+        if (user == null){
+            user=createUserWithPhone(loginForm.getPhone());
+        }
+        //TODO 把用户信息存到redis，
+        //随机生成字符作为token
+        String token = UUID.fastUUID().toString();
+
+        //改为dto传输
+        UserDTO userDTO = new UserDTO();
+        CopyOptions.create()
+                .setIgnoreNullValue(true)
+                .setFieldValueEditor((fieldName,fieldvalue)->fieldvalue.toString());
+
+        BeanUtils.copyProperties(user, userDTO);
+        //把对象转为hash存储
+        Map<String, Object> userMap = BeanUtil.beanToMap(userDTO, new HashMap<>(),
+                CopyOptions.create()
+                        .setIgnoreNullValue(true)
+                        .setFieldValueEditor((fieldName, fieldValue) -> fieldValue.toString()));
+        stringRedisTemplate.opsForHash().putAll(LOGIN_USER_KEY+token, userMap);
+
+        //设置token有效期
+        stringRedisTemplate.expire(LOGIN_USER_KEY+token,LOGIN_USER_TTL,TimeUnit.MINUTES);
+
+        //返回token
+        return Result.ok(token);
+
+    }
+
+    private User createUserWithPhone(String phone) {
+        User user = new User();
+        user.setPhone(phone);
+        user.setNickName(USER_NICK_NAME_PREFIX+RandomUtil.randomString(10));
+        save(user);
+        return user;
+    }
 }
